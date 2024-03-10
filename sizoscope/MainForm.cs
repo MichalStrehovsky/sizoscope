83d7d3cba299b19bd3b64526f0c5e6b4a6004ac4
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2024-03-10 20:52:42 +0900

    Random fixes (#11)

--- sizoscope/MainForm.cs
+++ sizoscope/MainForm.cs
@@ -163,7 +163,12 @@ namespace sizoscope
                     AddTypes(form, t.GetNestedTypes());
 
                     if (form._searchComboBox.SelectedIndex is 0 or 2)
+                    {
+                        foreach (var s in t.GetTypeSpecifications())
+                            AddMembers(form, s.GetMembers());
+
                         AddMembers(form, t.GetMembers());
+                    }
                 }
             }
 
@@ -373,6 +378,32 @@ namespace sizoscope
                            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
                            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
                            THE SOFTWARE.
+
+                           License notice for TurboXml
+
+                                                      Copyright (c) 2024, Alexandre Mutel
+                           All rights reserved.
+
+                           Redistribution and use in source and binary forms, with or without modification
+                           , are permitted provided that the following conditions are met:
+
+                           1. Redistributions of source code must retain the above copyright notice, this 
+                              list of conditions and the following disclaimer.
+
+                           2. Redistributions in binary form must reproduce the above copyright notice, 
+                              this list of conditions and the following disclaimer in the documentation 
+                              and/or other materials provided with the distribution.
+
+                           THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+                           ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+                           WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+                           DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+                           FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+                           DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+                           SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+                           CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+                           OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+                           OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
                            """
                 },
                 Text = """
